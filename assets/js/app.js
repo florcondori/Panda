1991c0ddb97ae7f2959c8d773f8232fe209725be
flor condori <condorihuamanif@gmail.com>
2017-04-17 16:37:36 -0500

    agregando las img dinamicamente

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,29 @@ window.addEventListener("load", function(){
 						{name:"Oso Panda en el Arbol", url:"panda_arbol.jpg"},
 						{name:"Panda con su hijo", url:"panda_hijo.jpg"},
 						{name:"Oso Panda sentado", url:"panda_sentado.jpg"}];
+
+	var seccionFotos = document.getElementById("pandas");
+	seccionFotos.appendChild(cargarImagenes(arrayImagenes));
 });
+
+function cargarImagenes(arrayImagenes){
+	var documentFragment = document.createDocumentFragment();
+	arrayImagenes.forEach(function(elem){
+		var div = document.createElement("div");
+		div.classList.add("div-contenedor");
+
+		var img = document.createElement("img");
+		img.setAttribute("src", "assets/img/"+elem.url);
+		img.setAttribute("alt", elem.name);
+		div.appendChild(img);
+
+		var btn = document.createElement("button");
+		btn.innerHTML = "X";
+		btn.setAttribute("class", "js-btn-cerrar-img");
+		div.appendChild(btn);
+
+		documentFragment.appendChild(div);
+	});
+
+			return documentFragment;
+}
