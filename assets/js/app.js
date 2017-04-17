2a7f10559e5cd14f224f812388fd5b5ff5f55748
flor condori <condorihuamanif@gmail.com>
2017-04-17 16:08:38 -0500

    propiedades en css para una foto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-app
+window.addEventListener("load", function(){
+	var arrayImagenes = [{name:"Oso Panda", url:"Oso_panda.png"},
+						{name:"Oso Panda en el Arbol", url:"panda_arbol.jpg"},
+						{name:"Panda con su hijo", url:"panda_hijo.jpg"},
+						{name:"Oso Panda sentado", url:"panda_sentado.jpg"}];
+});
